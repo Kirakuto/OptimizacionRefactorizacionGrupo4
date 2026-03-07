08ba1d8559db03601a7e4688aacffffa3123999a
jossessanchez <jossessanchez.dm.social@gmail.com>
2026-03-07 13:44:46 +0100

    Add files via upload

---
+++ src/grupo4entornos/LogUtils.java
@@ -0,0 +1,16 @@
+package grupo4entornos;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author USUARIO
+ */
+public class LogUtils {
+    public static void logError(String msg) {
+        System.out.println("[LOG DISCO]: " + msg);
+    }
+}
