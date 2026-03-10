d7599819db92e9c47186b2f607dd5dd62b004dbd
Unai718 <130696359+Unai718@users.noreply.github.com>
2026-03-10 20:00:33 +0100

    Add files via upload

---
+++ SistemaTienda.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entornos2;
+import java.util.ArrayList;
+/**
+ *
+ * @author WEB1-32
+ */
+public class SistemaTienda {
+    private ArrayList<Producto> productos = new ArrayList<>();
+
+    public void venderProducto(String nombre, int cantidad) {
+        for (Producto i : productos) {
+            if (i.getNombre().equals(nombre)) {
+                // DUPLICACIÓN DE LÓGICA: Verificación de stock
+                if (i.getStock() >= cantidad) {
+                    i.setStock(i.getStock()-cantidad);
+                    double total = calcularPrecioFinal(cantidad*i.getPrecio());
+
+                    System.out.println("Venta realizada: " + total);
+                } else {
+                    System.out.println("Error: No hay stock suficiente");
+                }
+            }
+        }
+    }
+    
+    private double calcularPrecioFinal(double base) {
+        return base * 1.21;
+    }
+}
