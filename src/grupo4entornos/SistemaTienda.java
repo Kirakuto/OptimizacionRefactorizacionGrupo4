16e49007cd0ba9a3ab0c1ca6f84a9ec92c738a02
Nai35 <53799798+Nai35@users.noreply.github.com>
2026-03-16 15:38:28 +0100

    Sistema tienda Arreglado

--- src/grupo4entornos/SistemaTienda.java
+++ src/grupo4entornos/SistemaTienda.java
@@ -1,58 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package grupo4entornos;
 
-/**
- *
- * @author WEB1-32
- */
+package grupo4proyecto;
+import java.util.ArrayList;
+
 public class SistemaTienda {
-
-    // Listas paralelas (Olor: Primitive Obsession)
-    private String[] productos = {"Camiseta", "Pantalon", "Zapatos"};
-    private int[] stocks = {50, 20, 10};
-    private double[] precios = {15.99, 29.99, 59.99};
-
-    private double calcularPrecioFinal(double base) {
-        return base * 1.21;
-    }
+    private ArrayList<Producto> ProductosTienda = new ArrayList<>();
 
     public void venderProducto(String nombre, int cantidad) {
-        for (int i = 0; i < productos.length; i++) {
-            if (productos[i].equals(nombre)) {
+        for (Producto i : ProductosTienda) {
+            if (i.getNombre().equals(nombre)) {
                 // DUPLICACIÓN DE LÓGICA: Verificación de stock
-                if (stocks[i] >= cantidad) {
-                    stocks[i] -= cantidad;
-                    double total = cantidad * precios[i];
+                if (i.getStock() >= cantidad) {
+                    i.setStock(i.getStock()-cantidad);
+                    double total = calcularPrecioFinal(cantidad*i.getPrecio());
 
-                    // Lógica de Impuestos (Duplicada en generarFactura)
-                    double conIva = calcularPrecioFinal(total);
-
-                    System.out.println("Venta realizada: " + conIva);
+                    System.out.println("Se ha realizado la venta: " + total);
                 } else {
-                    System.out.println("Error: No hay stock suficiente");
+                    System.out.println("Error: El stok del producto no es suficiente");
                 }
             }
         }
     }
-
-    public void generarFactura(String nombre, int cantidad) {
-        for (int i = 0; i < productos.length; i++) {
-            if (productos[i].equals(nombre)) {
-                double base = cantidad * precios[i];
-
-                // DUPLICACIÓN DE LÓGICA: El cálculo del IVA es el mismo
-                double totalFactura = calcularPrecioFinal(base);
-
-                System.out.println("FACTURA: " + nombre + " Cant: " + cantidad + " Total: " + totalFactura);
-            }
-        }
-    }
-
-    public void logError(String msg) {
-        // Este método no pertenece a la lógica de tienda (Falta de cohesión)
-        System.out.println("[LOG DISCO]: " + msg);
+    
+    private double calcularPrecioFinal(double base) {
+        return base * 1.21;
     }
 }
